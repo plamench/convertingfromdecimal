9d4219fd15bf1ed9d957134a997db0409ddff161
plamench <35867018+plamench@users.noreply.github.com>
2018-02-09 21:37:53 -1200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int>();
+            nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int N = (int)nums[0];
+            int number = nums[1];
+            int remainder;
+            string result = null;
+            if (N >= 2 && N <= 10)
+            {
+                while (number > 0)
+                {
+                    remainder = number % N;
+                    number /= N;
+                    result = remainder.ToString() + result;
+
+                }
+                Console.WriteLine(result);
+
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
